d3ae6df0c1afa99850a7f64156747d66e5e6f952
Nikolay-23 <115502655+Nikolay-23@users.noreply.github.com>
2025-07-16 18:25:59 +0000

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using BookAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    // https://localhost:7040/api/books
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private Book[] _books = new Book[]
+        {
+            new Book{Id = 1, Author = "Author One", Title = "Book One"}
+        };
+    }
+}
